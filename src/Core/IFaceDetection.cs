3a0e36bbf195fec2ccaa113ac2521939f9cb78a3
Coen Munckhof <c.munckhof@gmail.com>
2020-02-18 23:21:54 +0100

    Initial commi

---
+++ src/Core/IFaceDetection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core
+{
+    public interface IFaceDetection
+    {
+        string Name { get; }
+
+        int Process(string inputFilename, string outputDirectory);
+    }
+}
---
+++ src/FaceDetection.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Core;
+using FaceDetection.OpenCv;
+
+namespace FaceDetection.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var algorithms = new List<IFaceDetection>
+            {
+                new OpenCvDnnCaffe(),
+                new OpenCvDnnTensorflow(),
+            };
+
+            var img = "liverpool.jpg";
+
+            foreach (var algo in algorithms)
+            {
+                var faceCount = algo.Process(img, "samples\\");
+                System.Console.WriteLine($"{algo.Name} found {faceCount} faces");
+            }
+
+            System.Console.WriteLine("Done");
+            System.Console.ReadLine();
+        }
+    }
+}
